f3d368ed7e289e6894c9e19ad1648def6244aac9
subham-git55 <55subhambehera@gmail.com>
2024-07-09 11:03:23 +0530

    adding my first program

---
+++ Subham/second program.py
@@ -0,0 +1,32 @@
+#add two number
+a= 9
+b= 10
+print(a+b)
+
+#substract two number
+
+b = 7
+c = 5
+print(b-c)
+
+#multiply two number
+
+v= 5
+x= 5
+print(v*x)
+
+# five time integer number
+
+a= ("subham")
+print("subham"*5)
+
+
+# average number
+
+b=40
+c=30
+d=10
+print("average:",(b+c+d)//3)
+
+
+
