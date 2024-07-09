7f2b2799a2195b5aba4f6eefdf00ab116d49c32f
subham-git55 <55subhambehera@gmail.com>
2024-07-09 14:24:01 +0530

    adding my first program

--- Subham/second program.py
+++ Subham/second program.py
@@ -28,5 +28,67 @@ c=30
 d=10
 print("average:",(b+c+d)//3)
 
+#squre and cube
+
+s = 4
+print("square:",s**4)
+print("cube:",s**3)
+
+#interchanger values between varibules
+
+a=50
+b=60
+a,b = b,a
+print("a =", a)
+print("b =", b)
+
+#math formula
+#(a+b)^2 = a^2 b^2+2ab
+a=6
+b=4
+lhs=(a+b)**2
+rhs=a**2+b**2+2*a*b
+print("lhs:", lhs)
+print("rhs:", rhs)
+print (lhs == rhs)
+
+
+#math formula
+#(a – b)2 = a^2 + b^2 – 2ab
+a=8
+b=5
+lhs=(a-b)**2
+rhs=a**2+b**2-2*a*b
+print("lhs:", lhs)
+print("rhs:", rhs)
+print (lhs == rhs)
+
+#math formula
+#a2 – b2 = (a-b)(a+b)
+a=9
+b=5
+lhs=a**2-b**2
+rhs=a-b*a+b
+print("lhs:", lhs)
+print("rhs:", rhs)
+print (lhs == rhs)
+
+#math formula
+# (a – b)3 = a3 – 3a2b + 3ab2 – b3
+a=8
+b=4
+lhs=a-b**3
+rhs=a**3-3*a*2*b+3*a*b**2-b**3
+print("lhs:", lhs)
+print("rhs:", rhs)
+print (lhs == rhs)
+
+#area sqaure
+#area = a*a
+
+
+
+
+
 
 
