583750bdfc316eadf8f98f83e558549f96e299bb
Kalpashree-Mallesh <kalpa.ec38@gmail.com>
2024-07-11 21:18:49 +0530

    updated the python file

--- Kalpashree/PythonProgramm/BasicPrograms/first_20Programs.py
+++ Kalpashree/PythonProgramm/BasicPrograms/first_20Programs.py
@@ -1,7 +1,7 @@
 ####Addition of two Integer####
-a = 40
-b = 50
-print("Sum of two numbers:", a+b)
+a1 = 40
+b1 = 50
+print("Sum of two numbers:", a1+b1)
 
 
 ####Subtraction of two numbers####
@@ -27,7 +27,39 @@ print("Average of given number:",p1+q1+r1/3)
 #### find the median ######
 list = [30,40,70,10,80,60,20]
 list.sort()
-a1 = (len(list))/2
-print("Median:",list[int(a1)])
+a2 = (len(list))/2
+print("Median:",list[int(a2)])
 
+#### Square and Cube of a number#######
+num1 = 5
+print('Square:',num1**2)
+print('Cube:',num1**3)
+
+####interchange the value#####
+J = 10
+K = 20
+J,K = K,J
+print("Value of J:",J)
+print("Value of K:",K)
+
+####Pythagorous theorem###
+#a^2+b^2=c^2
+import math
+p2 = 4 #one side value
+q2 = 5 #other side value
+hypo = p2**2+q2**2
+print("hypotenious side:",math.sqrt(hypo))
+
+
+####math Formula : (a + b)2 = a^2 + b^2 + 2ab####
+a = 8
+b = 3
+result =a**2+b**2+2*a*b
+print("(a + b)2:",result)
+
+#### formaula (a – b)2 = a^2 + b^2 – 2ab ####
+a2 = 5
+b2 = 2
+result =a2**2+b2**2-2*a2*b2
+print("(a2 + b2)2:",result)
 
