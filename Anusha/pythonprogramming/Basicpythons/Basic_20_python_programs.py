89f65f93ebfc6ff461bfe1177d0ac03211bfb87d
anushadhani <anudhani18@gmail.com>
2024-07-15 15:00:12 +0530

    Adding Basic python variable programs

---
+++ Anusha/pythonprogramming/Basicpythons/Basic_20_python_programs.py
@@ -0,0 +1,65 @@
+#1 Program to add two integers
+a = 100
+b = 200
+print(a+b)  #300
+
+#2 program to subtract two integers
+a=50
+b=40
+print(a-b) #10
+
+#3 program to multiply two integers
+a=1000
+b=40
+print(a*b) #40000
+
+#4program to divide two integers by single slash /
+a=45
+b=8
+print(a/b) #5.6
+
+#5 program to divide two integers by double slash //
+a=45
+b=8
+print(a//b) #5
+
+
+#6 Program to repeat a given string 5 times.
+str_1="anusha"
+print(str_1*5)
+
+
+#7 Program to get the Average of given numbers.
+a=20
+b=30
+c=40
+print("Average:",a+b+c/3)  #63.333
+
+
+
+#8 Print the square and cube of a given number.
+numb=20
+print("sqaure:",numb**2) #400
+print("cube:",numb**3)#8000
+
+
+#9 Program to interchange values between variables.
+a=3
+b=4
+a,b=b,a
+print("value of a:",a)    # value of a:4
+print("value of b:",b)    # value of a:3
+
+#10 Program to display some of two complex numbers
+c1=2.5+2.5j
+c2=3.0-0.5j
+c3=c1+c2
+print("some of complex no.:",c3) #(5.5+2j)
+
+
+#solve the given math formula.Formula : (a + b)2 = a^2 + b^2 + 2ab
+a = 2
+b = 3
+result = a**2+2*a*b+b**2
+print("(a+b)^2: ",result)
+
