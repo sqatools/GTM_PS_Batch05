27447b981ae4971acc9578b666e575f8a2ab4a37
Shreyasbh0591 <shreyasbhokare43@gmail.com>
2024-07-16 22:37:39 +0530

    add basic program

---
+++ shreyash/BasicPrograms.py
@@ -0,0 +1,84 @@
+#Python Program to add two integer values
+#a = 3
+#b = 4
+#print(a+b)
+
+#Python Program to subtract two integer values
+#a=10
+#b=3
+#print(a-b)
+
+#Program to multiply two numbers
+#a=6
+#b=7
+#print(a*b)
+
+#Python program to repeat a given string 5 times
+#str1 = "SQATools"
+#print(str*5)
+
+
+#Program to get the Average of given numbers
+#a=40
+#b=50
+#c=30
+#print((a+b+c)/3, "Average: ")
+
+# program to get the median of given numbers.
+#Note: all the numbers should be arranged in ascending order
+#Formula : (n+1)/2
+#list1 =[45, 60, 61, 66, 70, 77, 80]
+#list1.sort()
+#a = (len(list1))/2
+#print(list1[int(a)], "Median: ")
+
+#program to print the square and cube of a given number
+#num = 9
+#print("Square: ",num**2)
+#print("Cube: ",num**3)
+
+#Program to interchange values between variables
+#a=10
+#b=20
+#a,b=b,a
+#print("a:,b=b:,a")
+
+#Python program to solve this Pythagorous theorem.
+#Theorem : (a2 + b2 = c2)
+#a = 5 # site1 value
+#b = 4 # site2 value
+
+#C = a**2+b**2
+#print("c: ", math.sqrt(c))#
+
+#program to solve the given  math formula.
+#Formula : (a + b)2 = a^2 + b^2 + 2ab
+#a=5
+#b=5
+#result = a**2+b**2+2a*b
+#print("(a+b)^2: ",result)#
+
+#program to solve the given math formula.
+#Formula : a2 – b2 = (a-b)(a+b)
+#a=4
+#b=3
+#result = (a-b)*(a+b)
+#print("(a^2-b^2): ",result)
+
+#Python program to calculate the area of the square.
+#Formula: area = side^2
+#side = int(input("Enter the side of a square:20 "))
+#print("Area of sqaure: ",side**2)
+
+#program to calculate the area of a circle.
+#Formula = PI*r*r
+#radius = int(input("Enter the radius of circle: "))
+#area = (3.14*radius*radius)
+#print("Area of circle: ", area)
+
+#program to find the sum of natural numbers
+#num = int(input("Enter a number up to which you want to find sum: "))
+#total = 0
+#for i in range(1,num+1):
+#    total += i
+#print("Total: ",total)
