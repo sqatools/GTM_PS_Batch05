3d8ebd541c450e1ea90813870188256bf1379d98
ShwethaGithub123 <shwethapoojary480@gmail.com>
2024-07-17 19:06:50 +0530

    adding python practice file for If else

---
+++ ShwethaPoojary/python_conditional/Python_15_July.py
@@ -0,0 +1,17 @@
+print("-"*30)
+a=50
+b=60
+
+if a==b:
+    print("Value A and B are Equal")
+else:
+    print("A and B are not Equal")
+
+print("-"*30)
+a=50
+b=50
+
+if a==b:
+    print("Value A and B are Equal")
+else:
+    print("A and B are not Equal")
