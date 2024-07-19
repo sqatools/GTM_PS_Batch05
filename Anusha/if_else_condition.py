d60677bee0fa456af490d797758c5f7b9c7b6b1d
anushadhani <anudhani18@gmail.com>
2024-07-19 19:42:21 +0530

    Updating Basic python ifelse programs

--- Anusha/if_else_condition.py
+++ Anusha/if_else_condition.py
@@ -87,9 +87,88 @@ else:
     print("Given number is not divisible by 3 and 5")
 
 #10 Python program to print the square of the number
-print("-"*50)
-numb = 15
-if numb % 3 == 0 and numb % 5 == 0:
-    print("Given number is divisible by 3 and 5")
+'''print("-"*50)
+numb = int(input("Enter the number"))
+if numb % 11==0:
+    print(numb**2)
 else:
-    print("Given number is not divisible by 3 and 5")
+    print("no is not divisible by 11")
+
+#11 using input function print temperature
+print("-"*50)
+temperature =float(input("Enter temperature in celcius"))
+print(temperature)'''
+
+#12 Program to check the authentication of username and password
+'''name=input("Enter username:")
+Password=input("enter password:")
+if name == Password:
+    print("BOth are valid")
+else:
+    print("NOt valid")
+'''
+'''#13 Program to check whether a person eligible to vote
+print("-"*50)
+x = int(input("Enter Age:"))
+if x >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+'''
+
+#14 Check whether the given number is positive or not.
+print("-"*50)
+#x = int(input("Enter number:"))
+if x > 0:
+    print("positive number")
+else:
+    print("negative number")
+
+ #15 Check whether the given number is positive or not.
+print("-"*50)
+#x = int(input("Enter number:"))
+if x < 0:
+    print("positive number")
+else:
+    print("negative number")
+'''
+#16 Check whether a given character is uppercase or not.
+char = input("Enter a character: ")
+if char.isupper():
+    print("Uppercase")
+else:
+    print("lower case")
+
+'''
+#17 Print a square or cube of the given number
+'''num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print("Square: ", num ** 2)
+elif num % 3 == 0:
+    print("Cube: ", num ** 3)
+
+#18 Check whether the character is lowercase or not.
+#char = input("Enter a character: ")
+if char.islower():
+    print("Uppercase")
+else:
+    print("lower case")
+'''''
+
+#19 Program to check whether the number is float
+print("-" * 50)
+num1=3.5
+if type(num1) == float:
+    print("True")
+else:
+    print("False")
+
+#20 Program to check whether an alphabet is a vowel
+print("-" * 50)
+q = input("Enter a character: ")
+vowel = ["A","E","I","O","U","a","e","i","o","u"]
+
+if q in vowel:
+    print("Alphabet is a vowel")
+else:
+    print("Alphabet is not a vowel")
