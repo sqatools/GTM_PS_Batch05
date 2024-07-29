9e3045f7ce1e24573febff04826b96e0a444334d
Kalpashree-Mallesh <kalpa.ec38@gmail.com>
2024-07-29 18:21:46 +0530

    updated the python file

--- Deepesh/PythonProgramming/PythonString/Python_string_22July24.py
+++ Deepesh/PythonProgramming/PythonString/Python_string_22July24.py
@@ -1,3 +1,4 @@
+
 """
 Properties
 -> string is immutable data type
@@ -66,7 +67,7 @@ print(str_a[2: -2])  # od Morni
 # as per the rule output can not contain char from negative to positive index
 print(str_a[-1: 2]) # Noi output
 
-str_b = "Python Programming"
+str_b = ("Python Programming")
 print(str_b[4:8])  # on P
 print(str_b[2:-5]) # thon Progra
 print(str_b[-8: -2]) # grammi
@@ -104,7 +105,8 @@ print(str1[3 :10: 1])  # Are Lea
 print(str1[1:12:2])  # eAeLan
 
 # get values with negative indexing
-print(str1[-1:-10:-1])  # nohtyP gn
+print(str1[-1:-10:-1
+      ])  # nohtyP gn
 
 print(str1[4:-2:1]) # re Learning Pyth
 
