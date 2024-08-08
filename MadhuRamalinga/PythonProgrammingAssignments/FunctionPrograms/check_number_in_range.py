30b5eac4fe6580f3707fe5e9041a1670c980a97d
Madhu131524 <madhu.ramalinga1524@gmail.com>
2024-08-08 18:14:32 -0500

    Assignment - Function Programs

---
+++ MadhuRamalinga/PythonProgrammingAssignments/FunctionPrograms/check_number_in_range.py
@@ -0,0 +1,15 @@
+# Program to check whether a number is in a given range
+
+def check(num):
+    if n in range(1, 10):
+        print("True")
+    else:
+        print("False")
+n = int(input("Enter the number: "))
+check(n)
+
+# Enter the number: 9
+# True
+
+# Enter the number: 10
+# False
