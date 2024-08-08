945703a02ee6c6dc80db87179f9019952ed6f279
Madhu131524 <madhu.ramalinga1524@gmail.com>
2024-08-08 16:25:58 -0500

    Assignment - List Programs

--- MadhuRamalinga/PythonProgrammingAssignments/FunctionPrograms/addition.py
+++ MadhuRamalinga/PythonProgrammingAssignments/FunctionPrograms/addition.py
@@ -4,7 +4,7 @@ def addition():
     a = 100
     b = 200
     result = a + b
-    print("Addition od two numbers is:", result)
+    print("Addition od two numbers is:", result) # Addition od two numbers is: 300
 
 addition()
 
@@ -13,18 +13,22 @@ print("_"*50)
 def addition():
     x = 10
     y = 10
-    print("x + y is:", x+y)
+    print("x + y is:", x+y) # x + y is: 20
 
 addition()
 
 ########## OR ###########
 print("_"*50)
 
-''' def add(a,b):
+def add(a,b):
     total = a+b
     print("Total: ", total)
 
 num1 = int(input("Enter num1: "))
 num2 = int(input("Enter num2: "))
 
-add(num1, num2) '''
+add(num1, num2)
+
+# Enter num1: 2
+# Enter num2: 3
+# Total:  5
