78c6febbb07061c8c48d79cd66dda666cf2157dc
Shreyasbh0591 <shreyasbhokare43@gmail.com>
2024-08-08 13:28:58 +0530

    push string program

---
+++ shreyash/StringProgram.py
@@ -0,0 +1,14 @@
+#Q1: My name is shreyas and my ages is 32 and living in delhi
+
+Name="Shreyas"
+Age= "32"
+City= "delhi"
+
+#string concatenation
+#result= My name+"Name+" and my age is "+age+"and living in "+city+"
+#print("result")
+
+#Format Mrethod
+#result2= "My name is{} and my age{} and living in{}".format(Name,age,city)
+#print("result: ", result2)
+
