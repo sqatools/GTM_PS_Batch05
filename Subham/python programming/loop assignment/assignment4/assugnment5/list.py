6bada9632cfe28ef9822469d67ec2c63113956dc
subham-git55 <55subhambehera@gmail.com>
2024-08-09 14:38:47 +0530

    list methods

---
+++ Subham/python programming/loop assignment/assignment4/assugnment5/list.py
@@ -0,0 +1,14 @@
+# Problem to add elements from list1 to list2
+
+list1 = [1,2,5]
+list2 = [2,5,8]
+result = list1+list2
+print(result)
+
+print("_" * 50)
+
+# Python program to calculate the sum of all elements from a list
+
+list3 = [10,20,30,48,66]
+total =sum(list3)
+print (total)
