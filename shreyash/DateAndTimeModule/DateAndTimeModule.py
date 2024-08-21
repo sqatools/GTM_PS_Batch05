e29aa0e66c97fe88ee0cc8a1a3110562a49f852b
Shreyasbh0591 <shreyasbhokare43@gmail.com>
2024-08-21 12:12:52 +0530

    pushing date and time

---
+++ shreyash/DateAndTimeModule/DateAndTimeModule.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+##print(datetime.now().date())  #2024-08-21
+
+#print(datetime.now())           #2024-08-21 12:07:44.207857
+
+#print(datetime.now().month)     #8
+
+#print(datetime.now().year)       #2024
+
+#print(datetime.now().time())      #12:10:16.137240
+
+print(datetime.now().day)         #21
