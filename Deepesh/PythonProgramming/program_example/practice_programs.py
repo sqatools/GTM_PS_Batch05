2aa9eb1db50cb555966187b2ec60b87d57cee249
Bi12Python <xovek11875@bitvoo.com>
2024-09-10 20:19:26 +0530

    XPath Guide Advance Me.

---
+++ Deepesh/PythonProgramming/program_example/practice_programs.py
@@ -0,0 +1,12 @@
+# str1 = "Rahul Mohit John Rahul Vaibhav John"
+# str_list = str1.split()
+# output = []
+#
+# for word in str_list:
+#   if word not in output:
+#     output.append(word)
+#
+# print(output)
+
+text = 'Hello World'
+print(text.join('-'))
