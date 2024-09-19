db7758c03fc4d6fa5644fb842703e6be5bc2cf4f
Bi12Python <xovek11875@bitvoo.com>
2024-09-19 21:23:14 +0530

    headless mode execution

---
+++ Deepesh/PytestFrameowrk/test_smoke_cases.py
@@ -0,0 +1,27 @@
+import pytest
+
+def testfd_addition():
+    a1 = 50
+    b1 = 40
+    c1 = 30
+    assert a1 +b1 + c1
+
+def testfd_multiplication():
+    a1 = 50
+    b1 = 40
+    c1 = 30
+    assert a1 +b1 + c1
+
+def testfd_addition_subtraction():
+
+    a1 = 50
+    b1 = 40
+    c1 = 30
+    assert a1 -b1 - c1
+
+
+def testfd_divisionj():
+
+    a1 = 20
+    b1 = 4
+    assert a1/b1 == 5
