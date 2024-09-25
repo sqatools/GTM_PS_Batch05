8b36c5980d7befd776596974b475bc2941b31cae
subham-git55 <55subhambehera@gmail.com>
2024-09-25 20:16:37 +0530

    Merge remote-tracking branch 'origin/main'

---
+++ Subham/pytest_framework/test_program_pytest.py
@@ -0,0 +1,23 @@
+import pytest
+
+
+
+def test_addition():
+    a=10
+    b=20
+    c=30
+    assert (a+b+c) == 60
+
+def test_subtraction():
+    a = 10
+    b = 20
+    c = 30
+    assert (a - b - c) == 20
+
+def test_multiplication():
+    a = 10
+    b = 20
+    c = 30
+    assert (a*b*c) == 6000
+
+
