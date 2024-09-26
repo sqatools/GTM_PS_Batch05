91e50360f9dc1fcf6ed5d26cda7f51272e6a6ef1
Bi12Python <xovek11875@bitvoo.com>
2024-09-26 21:17:22 +0530

    test class

---
+++ Deepesh/Pytest-selenium/resource_data_file.py
@@ -0,0 +1,8 @@
+first_name_value = 'Rahul'
+last_name_value = 'Gupta'
+dob = "09/04/1990"
+add_more_pass_details = "Add 3 more passenger (200%)"
+source_city = "Kolkata"
+dest_city = "Chennai"
+depart_date = "09/29/2024"
+return_date = "10/05/2024"
