422fa4c932407733091556d302846f5c71a90af8
Madhu131524 <madhu.ramalinga1524@gmail.com>
2024-10-13 21:54:41 -0500

    Amazon Website Automation

--- MadhuRamalinga/Automation_Framework_with_seperate_folders/modules/amazon_website/amazon_website_page_class.py
+++ MadhuRamalinga/Automation_Framework_with_seperate_folders/modules/amazon_website/amazon_website_page_class.py
@@ -26,3 +26,41 @@ class AmazonWebsite(SeleniumBase):
 
     def verify_email_id(self):
         self.click_element(verify_email)
+
+    def existing_cust_sign_in(self, existing_cust_email_id, ex_cust_password_field):
+        self.click_element(existing_customer)
+        self.send_text(existing_cust_email, existing_cust_email_id)
+        self.click_element(continue_button)
+        self.send_text(existing_cust_password, ex_cust_password_field)
+        self.click_element(sign_in_button)
+
+    def edit_profile_information(self, enter_apt_no):
+        self.click_element(account_lists)
+        self.click_element(your_addresses)
+        self.click_element(edit_address)
+        self.send_text(edit_apt_no, enter_apt_no)
+
+    def search_for_products1(self, enter_product1):
+        self.send_text(search_amazon1, enter_product1)
+        self.click_element(search_entered_product1)
+        #self.click_element(filter_by_prime)
+        self.click_element(filter_by_brands)
+        #self.click_element(sort_by)
+
+    def add_items_to_cart(self):
+        self.click_element(selected_product1)
+        self.click_element(add_to_cart1)
+
+    def search_for_products2(self, enter_product2):
+        self.send_text(search_amazon2, enter_product2)
+        self.click_element(search_entered_product2)
+        self.click_element(selected_product2)
+        self.click_element(add_to_cart2)
+        self.click_element(go_to_cart)
+        self.click_element(remove_item)
+
+
+
+
+
+
--- MadhuRamalinga/Automation_Framework_with_seperate_folders/tests/amazon_website/test_amazon_website.py
+++ MadhuRamalinga/Automation_Framework_with_seperate_folders/tests/amazon_website/test_amazon_website.py
@@ -12,12 +12,24 @@ class TestAmazonWebsite:
 
     def test_amazon_website(self):
         self.driver.get("https://www.amazon.com/")
-        self.amazon_website.new_user_account()
+        time.sleep(5)
+
+        '''self.amazon_website.new_user_account()
         self.amazon_website.create_amazon_account()
         self.amazon_website.enter_name('Madhu')
         self.amazon_website.enter_email_id('madhu.ramalinga1524@gmail.com')
         self.amazon_website.enter_password('MadhuTesting')
         self.amazon_website.reenter_password('MadhuTesting')
-        self.amazon_website.verify_email_id()
-        #time.sleep(5)
-        #self.driver.close()
+        self.amazon_website.verify_email_id()'''
+
+        self.amazon_website.existing_cust_sign_in('madhu.ramalinga1524@gmail.com', 'MadhuTesting')
+        self.amazon_website.edit_profile_information('1538')
+        self.amazon_website.search_for_products1('dog toys')
+        self.amazon_website.add_items_to_cart()
+        self.amazon_website.search_for_products2('dog treats')
+
+        time.sleep(5)
+
+
+#command - python -m pytest -v -s .\tests\amazon_website\test_amazon_website.py
+
